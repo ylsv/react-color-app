f1e71486d7eb907ff5edbc10d00ff8750558648b
ylsv <lisov1988@gmail.com>
2020-04-26 11:17:44 +0300

    add browser-router and pallete route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-function App() {
-	return (
-		<div>
-			<Palette palette={generatePalette(seedColors[4])} />
-		</div>
-	);
+class App extends Component {
+	findPalette(id) {
+		return seedColors.find((palette) => palette.id === id);
+	}
+	render() {
+		return (
+			<Switch>
+				<Route exact path="/" render={() => <h1> palette list</h1>} />
+				<Route
+					exact
+					path="/palette/:id"
+					render={(routeProps) => (
+						<Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+					)}
+				/>
+			</Switch>
+
+			// <div>
+			// 	<Palette palette={generatePalette(seedColors[4])} />
+			// </div>
+		);
+	}
 }
 
 export default App;
