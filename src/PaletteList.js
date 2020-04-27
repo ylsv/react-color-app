b3722c09dc6744cd46796496b7e7d4d31a3088e6
ylsv <lisov1988@gmail.com>
2020-04-27 15:21:35 +0300

    add link to palette via history props

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -34,6 +34,9 @@ const styles = {
 };
 
 class PaletteList extends Component {
+	goToPalette(id) {
+		this.props.history.push(`/palette/${id}`);
+	}
 	render() {
 		const { palettes, classes } = this.props;
 		return (
@@ -42,7 +45,11 @@ class PaletteList extends Component {
 					<nav className={classes.nav}>
 						<h1>React Colors</h1>
 					</nav>
-					<div className={classes.palettes}>{palettes.map((palette) => <MiniPalette {...palette} />)}</div>
+					<div className={classes.palettes}>
+						{palettes.map((palette) => (
+							<MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />
+						))}
+					</div>
 				</div>
 			</div>
 		);
