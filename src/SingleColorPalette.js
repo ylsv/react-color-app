f9a001b8a7aa3cff161b6c0dd025f97d9e362e2c
ylsv <lisov1988@gmail.com>
2020-04-28 12:35:00 +0300

    add navbar and footer to singlecolorpalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 import ColorBox from './ColorBox';
 
 export default class SingleColorPalette extends Component {
 	constructor(props) {
 		super(props);
 		this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+		this.state = { format: 'hex' };
+		this.changeFormat = this.changeFormat.bind(this);
 	}
 	gatherShades(palette, colorToFilterBy) {
 		let shades = [];
@@ -13,16 +17,21 @@ export default class SingleColorPalette extends Component {
 			shades = shades.concat(allColors[key].filter((color) => color.id === colorToFilterBy));
 		}
 		return shades.slice(1);
-		// return all shades of given color
+	}
+	changeFormat(val) {
+		this.setState({ format: val });
 	}
 	render() {
+		const { format } = this.state;
+		const { paletteName, emoji } = this.props.palette;
 		const colorBoxes = this._shades.map((color) => (
-			<ColorBox key={color.id} name={color.name} background={color.hex} showLink={false} />
+			<ColorBox key={color.id} name={color.name} background={color[format]} showLink={false} />
 		));
 		return (
 			<div className="Palette">
-				<h1>Single Color Palette</h1>
+				<Navbar handleChange={this.changeFormat} showingAllColors={false} />
 				<div className="Palette-colors">{colorBoxes}</div>
+				<PaletteFooter paletteName={paletteName} emoji={emoji} />
 			</div>
 		);
 	}
