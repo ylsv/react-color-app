616d7621eca6fcd99418584ec80c12a03ef81e46
ylsv <lisov1988@gmail.com>
2020-05-14 19:14:35 +0300

    save palettes to LS

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { generatePalette } from './colorHelpers';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { palettes: seedColors };
+		const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+		this.state = { palettes: savedPalettes || seedColors };
 		this.savePalette = this.savePalette.bind(this);
 		this.findPalette = this.findPalette.bind(this);
 	}
@@ -18,7 +19,10 @@ class App extends Component {
 		return this.state.palettes.find((palette) => palette.id === id);
 	}
 	savePalette(newPalette) {
-		this.setState({ palettes: [ ...this.state.palettes, newPalette ] });
+		this.setState({ palettes: [ ...this.state.palettes, newPalette ] }, this.syncLocalStorage);
+	}
+	syncLocalStorage() {
+		window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes));
 	}
 	render() {
 		return (
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,9 +8,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
-import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import styles from './styles/PaletteFormNavStyles';
 
