ada2977a28e99fcb12ffe58585b6d1260ff20d34
ylsv <lisov1988@gmail.com>
2020-05-20 23:46:09 +0300

    clean up route animation

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -36,6 +36,7 @@ class PaletteMetaForm extends Component {
 	savePalette(emoji) {
 		const newPalette = { paletteName: this.state.newPaletteName, emoji: emoji.native };
 		this.props.handleSubmit(newPalette);
+		this.setState({ stage: '' });
 	}
 	handleClickOpen = () => {
 		this.setState({ open: true });
