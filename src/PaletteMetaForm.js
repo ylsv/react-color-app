aec0320e517875d793a0fb2b69f1b5bdb0957a5b
ylsv <lisov1988@gmail.com>
2020-05-21 15:44:54 +0300

    clean up files

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -43,16 +43,16 @@ class PaletteMetaForm extends Component {
 	};
 
 	render() {
-		const { newPaletteName } = this.state;
-		const { hideForm, handleSubmit } = this.props;
+		const { newPaletteName, stage } = this.state;
+		const { hideForm } = this.props;
 
 		return (
 			<div>
-				<Dialog open={this.state.stage === 'emoji'} onClose={hideForm}>
+				<Dialog open={stage === 'emoji'} onClose={hideForm}>
 					<DialogTitle id="form-dialog-title">Choose a Palette Emoji</DialogTitle>
 					<Picker title="Choose Your Emoji" onSelect={this.savePalette} />
 				</Dialog>
-				<Dialog open={this.state.stage === 'form'} onClose={hideForm} aria-labelledby="form-dialog-title">
+				<Dialog open={stage === 'form'} onClose={hideForm} aria-labelledby="form-dialog-title">
 					<DialogTitle id="form-dialog-title">Choose a Palette Name</DialogTitle>
 					<ValidatorForm onSubmit={this.showEmojiPicker}>
 						<DialogContent>
